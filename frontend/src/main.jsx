c43afe106ab3188e3c23015a412032597451fc5f
Ravikant <ravikant@orginv8.com>
2025-06-09 12:34:53 +0000

    changes in cat

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import './index.css';
 import App from './App.jsx';
 import store from './store/store.jsx'; 
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}> 
